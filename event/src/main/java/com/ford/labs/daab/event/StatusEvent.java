ff745df2868bc36fcb04d2f388e4995026a14426
Tanmay Satam <tsatam@ford.com>
2020-03-19 11:11:29 -0400

    Update copyright

--- event/src/main/java/com/ford/labs/daab/event/StatusEvent.java
+++ event/src/main/java/com/ford/labs/daab/event/StatusEvent.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019 Ford Motor Company
+ * Copyright (c) 2020 Ford Motor Company
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
